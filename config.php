b5a1d9486ce066f9668628bb7be1e0b0d1f1c503
paugichia-debug <paugichia@gmail.com>
2026-07-09 00:50:02 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,15 +5,11 @@ $password = getenv('DB_PASSWORD') ?: '';
 $database = getenv('DB_NAME') ?: '';
 $port = getenv('DB_PORT') ?: 3306;
 
-if (!$host || !$user || !$database) {
-    die("Database variables not set. Check DB_HOST, DB_USER, DB_NAME");
-}
-
 $conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo "Database connected successfully!";
+echo "Connected successfully!";
 ?>
