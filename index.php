c4b6f4eebe045eb3585102673cb0e9d9d89ea93f
paugichia-debug <paugichia@gmail.com>
2026-07-09 01:22:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-// Redirect to login page
+// Start session
+session_start();
+
+// Include database config
+include 'config.php';
+
+// If user is logged in, go to dashboard
+if (isset($_SESSION['user_id'])) {
+    if ($_SESSION['role'] == 'admin') {
+        header("Location: admin_dashboard.php");
+    } else {
+        header("Location: driver_dashboard.php");
+    }
+    exit;
+}
+
+// If not logged in, show login page
 header("Location: login.php");
 exit;
 ?>
